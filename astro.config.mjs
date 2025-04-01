8a361a63d42a383f262640f2a264071e4b79ea42
Cristina VG <alechrysalis@gmail.com>
2025-04-01 20:04:30 +0200

    base puesta de nuevo

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
     integrations: [react(), tailwind(), sitemap()],
     site: template.website_url,
+    base: template.base,
 });
